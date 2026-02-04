c75d1fd6ed52c87b747e1da11d1a6706d0ea1eb6
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2026-02-04 16:57:50 +0530

    fix: adjust padding in hero component for better spacing

--- app/components/home/hero.vue
+++ app/components/home/hero.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="lg:h-[90svh] h-[60svh] xl:px-[12em] lg:px-[5em] px-[1em] ">
-        <div class="text-center lg:py-[1em] py-[2em]">
+        <div class="text-center lg:py-[1em] py-[1.5em]">
             <h1 class="font-[sansita] lg:text-[40pt] text-[30pt]">Rooted in <span class="text-[#004b23]">Experience</span>. <br />
                 Driven by Sustainability.</h1>
             <p class="lg:text-[18pt] text-[15pt]">Combining fifteen years of expertise with advanced recycling technology to
                 deliver high-quality plastic
                 solutions for a cleaner tomorrow.</p>
-            <div class="md:py-[2em]">
+            <div class="md:py-[2em] py-[1em]">
                 <img src="~/assets/images/homebanner.jpg" alt="" class="w-[100%] object-contain shadow-sm rounded-[2em]">
             </div>
         </div>
