65e8b3f745a6abd9954c0df9fb9cb0ea5304ce97
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2026-02-05 19:19:42 +0530

    fix: refine layout and text size in hero component for improved readability

--- app/components/home/hero.vue
+++ app/components/home/hero.vue
@@ -1,16 +1,14 @@
 <template>
-    <div class=" ">
-        <div class="text-center lg:py-[1em] lg:h-[90svh] h-[60svh] bg-[#73a942] xl:px-[15em] lg:px-[5em] px-[1em]">
-            <h1 class="font-[sansita] lg:text-[40pt] text-[28pt]">Rooted in <span
-                    class="text-[#004b23]">Experience</span>. <br />
+    <div class="lg:h-[150svh] h-[85svh]">
+        <div class="text-center lg:py-[1em] lg:h-[90svh] bg-[#73a942] h-[60svh] xl:px-[12em] lg:px-[5em] px-[1em]">
+            <h1 class="font-[sansita] lg:text-[40pt] text-[25pt]">Rooted in <span class="text-[#004b23]">Experience</span>. <br />
                 Driven by Sustainability.</h1>
-            <p class="lg:text-[18pt] text-[15pt]">Combining fifteen years of expertise with advanced recycling
-                technology to
+            <p class="lg:text-[18pt] text-[15pt]">Combining fifteen years of expertise with advanced recycling technology to
                 deliver high-quality plastic
                 solutions for a cleaner tomorrow.</p>
-        </div>
-        <div class="lg:mt-[-30em] mt-[-4em] mb-[1em] xl:px-[15em] lg:px-[7em] px-[1em]">
-            <img src="~/assets/images/homebanner.jpg" alt="" class="w-full object-contain shadow-sm rounded-[2em]">
+            <div class="md:py-[2em] py-[1em]">
+                <img src="~/assets/images/homebanner.jpg" alt="" class="w-[100%] object-contain shadow-sm rounded-[2em]">
+            </div>
         </div>
     </div>
 </template>
