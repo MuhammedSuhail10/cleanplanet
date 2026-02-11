1b009078cf2beaef799c1e4f894878da9bad2942
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2026-02-11 23:39:26 +0530

    Clean Planet

--- app/components/links/footer.vue
+++ app/components/links/footer.vue
@@ -28,14 +28,14 @@ const currentYear = now.getFullYear();
             </div>
             <div>
                 <h5 class="font-[sansita] text-[#004b23] text-[20pt] text-center md:text-start">Contact Us</h5>
-                <div class="flex flex-col gap-[0.5em] items-center my-[1em] text-[15pt]">
+                <div class="flex flex-col gap-[0.5em] items-center md:items-start my-[1em] text-[15pt]">
                     <div class="flex gap-[0.5em] items-start">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
                             <path
                                 d="M20.621 8.45c-1.05-4.62-5.08-6.7-8.62-6.7h-.01c-3.53 0-7.57 2.07-8.62 6.69-1.17 5.16 1.99 9.53 4.85 12.28a5.436 5.436 0 0 0 3.78 1.53c1.36 0 2.72-.51 3.77-1.53 2.86-2.75 6.02-7.11 4.85-12.27Zm-8.62 5.01a3.15 3.15 0 1 1 0-6.3 3.15 3.15 0 0 1 0 6.3Z"
                                 fill="#000"></path>
                         </svg>
-                        <p class="cursor-pointer hover:text-[#004b23]">Clean Planet Recycle Pvt Ltd, Kunnathunad, <br /> Arackapady, Ernakulam</p>
+                        <p class="cursor-pointer hover:text-[#004b23] text-center md:text-start">Clean Planet Recycle Pvt Ltd, Kunnathunad, <br /> Arackapady, Ernakulam</p>
                     </div>
                     <div class="flex gap-[0.5em] items-start">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
